b0ac60a7eadfe1eb3f6e1bf7478a67f6bd6b0264
Fabio Dias <fabiowebmain@gmail.com>
2020-03-24 23:38:07 -0300

    Implementação do Material-UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 import './App.css';
+
+import Header  from "./components/Header/Header";
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <React.Fragment>
+          <CssBaseline />
+          <Container maxWidth="md">
+              <Header />
+              <Typography component="div" style={{ backgroundColor: '#fff', height: '100vh' }} />
+          </Container>
+      </React.Fragment>
   );
 }
 
