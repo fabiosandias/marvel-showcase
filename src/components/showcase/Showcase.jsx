37bcf0fec47eca46b64d4913f4fb3ba4e665a12c
Fabio Dias <fabiowebmain@gmail.com>
2020-03-27 20:08:41 -0300

    Inclusão do progress  loading e efeito ao carregar os cards

--- src/components/showcase/Showcase.jsx
+++ src/components/showcase/Showcase.jsx
@@ -6,6 +6,10 @@ import md5 from "js-md5";
 import {API} from "../../constants/constants";
 import axios from "axios";
 import {makeStyles} from "@material-ui/core/styles";
+import Progress from "../progress/Progress";
+
+import './Progress.css';
+import Grow from "@material-ui/core/Grow";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -21,29 +25,41 @@ const useStyles = makeStyles(theme => ({
 export default props => {
     const classes = useStyles();
     const [characters, setCharacters] = useState([]);
+    const [isLoading, setLoading] = useState(false);
 
     const timestamp = Number(new Date())
     const apiHash = md5.create();
     apiHash.update(`${timestamp}${API.PRIVATE_KEY}${API.PUBLIC_KEY}`);
 
-    useEffect( () => {
-         axios.get(`${API.URL}${API.METHOD.COMICS}?ts=${timestamp}&apikey=${API.PUBLIC_KEY}&hash=${apiHash.hex()}`)
-            .then(response => setCharacters(response.data.data.results));
+    useEffect(() => {
+        setLoading(true)
+        axios.get(`${API.URL}${API.METHOD.COMICS}?ts=${timestamp}&apikey=${API.PUBLIC_KEY}&hash=${apiHash.hex()}`)
+            .then(response => {
+                setCharacters(response.data.data.results);
+                setLoading(false);
+            });
     }, []);
 
     return (
         <>
-            <Typography component="div" style={{backgroundColor: '#fff'}}>
-                <div className={classes.root}>
-                    <Grid container spacing={3}>
-                        {characters.map(char =>
-                            <Grid key={char.id} item xs={12} md={3}>
-                                <Card comic={char}></Card>
-                            </Grid>
-                        )}
-                    </Grid>
+            {isLoading
+                ? <div className="progress-bar">
+                    <Progress></Progress>
                 </div>
-            </Typography>
+                : <Typography component="div" style={{backgroundColor: '#fff'}}>
+                    <div className={classes.root}>
+                        <Grid container spacing={3}>
+                            {characters.map(char =>
+                                <Grow in={!isLoading} key={char.id}>
+                                    <Grid item xs={12} md={3}>
+                                        <Card comic={char}></Card>
+                                    </Grid>
+                                </Grow>
+                            )}
+                        </Grid>
+                    </div>
+                </Typography>
+            }
         </>
     );
 }
